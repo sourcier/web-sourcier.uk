a882662b179d16f9763900b47290ec809eca3cf0
Roger Rajaratnam <roger@sourcier.uk>
2026-02-28 00:43:10 +0000

    Add Tailwind 4 plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
