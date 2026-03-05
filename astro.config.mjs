78c0c00f73a647899c47f6a2d20c64838cae49ca
Roger Rajaratnam <roger@sourcier.uk>
2026-03-05 22:32:55 +0000

    chore: remove preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import preact from "@astrojs/preact";
-
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()],
+  integrations: [],
 
   vite: {
     plugins: [tailwindcss()],
