b920e0ec82acdecdcda62b7f7c6de6fc2d50e105
Roger Rajaratnam <roger@sourcier.uk>
2026-03-09 23:20:37 +0000

    feat: update site URL in astro.config and remove Tailwind CSS dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://sourcier.uk",
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [],
   },
 });
