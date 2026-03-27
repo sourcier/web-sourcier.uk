64477647363d15bb80e8d0e51352e587ea7329f6
Roger Rajaratnam <roger@sourcier.uk>
2026-03-27 17:52:09 +0000

    feat: add FontAwesome icons for social links and implement icon utility function

---
+++ src/utils/icons.ts
@@ -0,0 +1,20 @@
+import { icon, type IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
+interface IconOptions {
+  class?: string;
+  size?: number;
+}
+
+export function faIcon(
+  iconDef: IconDefinition,
+  { class: className, size = 20 }: IconOptions = {}
+): string {
+  return icon(iconDef, {
+    attributes: {
+      "aria-hidden": "true",
+      width: String(size),
+      height: String(size),
+      ...(className ? { class: className } : {}),
+    },
+  }).html[0];
+}
