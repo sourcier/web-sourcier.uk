eb269994921c84e6b9a3f2cad4a55f4cd81bac77
Roger Rajaratnam <roger@sourcier.uk>
2026-03-27 17:44:05 +0000

    Replace FontAwesome icons with SVGs in Footer and Header components for improved performance and customization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 import emoji from "remark-emoji";
 
 export default defineConfig({
   site: "https://sourcier.uk",
-  integrations: [react()],
   markdown: {
     remarkPlugins: [[emoji, { emoticon: true, accessible: true }]],
     shikiConfig: {
